df0bac1c3108e3aaee13f08c8e3eeb06d44ac185
EJ Morgan <edwardmorgan@EJs-MacBook-Pro.local>
2018-03-24 23:29:46 -0400

    Updated for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./models");
-const routes = require("./routes");
+// const routes = require("./routes");
 
 const app = express();
 const PORT = 3001 || process.env.PORT;
@@ -9,7 +9,7 @@ const PORT = 3001 || process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("client/build"));
-app.use(routes);
+// app.use(routes);
 
 db.sync().then(() => {
   app.listen(PORT, () => {
